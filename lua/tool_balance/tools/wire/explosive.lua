3ecad03dbfc4f6b63be6810fee4391196e974baf
PLally <11771225+PLally@users.noreply.github.com>
2019-05-23 00:13:16 -0400

    add wait period after spawning explosives

--- lua/tool_balance/tools/wire/explosive.lua
+++ lua/tool_balance/tools/wire/explosive.lua
@@ -8,6 +8,8 @@ local config = {
     delayreloadtime = { min = 5, max = math.huge }
 }
 
+local EXPLOSIVE_WAIT_PERIOD = 5
+
 -- min and max values for gmod_wire_explosive
 local values = { 
     {}, --key
@@ -26,6 +28,14 @@ local function wrapWireExplosive()
     local WIRE_EXPLOSIVE = scripted_ents.GetStored( "gmod_wire_explosive" ).t
     WIRE_EXPLOSIVE.Setup = clampFunction(WIRE_EXPLOSIVE.Setup, values)
     
+    local explode = WIRE_EXPLOSIVE.Explode
+    WIRE_EXPLOSIVE.Explode = function( self, .. .)
+        local age = CurTime() - self:GetCreationTime()
+        if age < EXPLOSIVE_WAIT_PERIOD then return end
+
+        explode( self, ... )
+    end
+
     print("[CFC_Tool_Balance] wire/explosive loaded")
 end
 
