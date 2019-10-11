5c09d4e5b5d3bd33e91583671e45f619837a0137
Periapsises <45960263+Periapsises@users.noreply.github.com>
2019-10-10 22:05:17 -0400

    Refractor variable file into toolFile

--- lua/autorun/server/sv_init_tool_balance.lua
+++ lua/autorun/server/sv_init_tool_balance.lua
@@ -9,8 +9,8 @@ local _, directories = file.Find( baseDir .. "*", "LUA")
 
 for _, dir in ipairs(directories) do
     local files, _ = file.Find( baseDir .. dir .. "/*.lua", "LUA" )
-    for _, file in ipairs( files ) do
-        include( baseDir .. dir .. "/" .. file )
+    for _, toolFile in ipairs( files ) do
+        include( baseDir .. dir .. "/" .. toolFile )
     end
 end
 
