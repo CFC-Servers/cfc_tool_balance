6c6f7e8d131748471903f3ad4d6642a92ca7cb5a
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-13 00:46:16 -0800

    Merge turret muter, add lamp clamps

---
+++ lua/tool_balance/tools/base/lamp.lua
@@ -0,0 +1,34 @@
+-- base/lamp
+
+-- config
+local config = {
+    fov        = { min = 10, max = 170 },
+    distance   = { min = 64, max = 2048 },
+    brightness = { min = 0, max = 8 },
+}
+
+-- min and max values for gmod_lamp
+local values = config
+
+local clampMethod = cfcToolBalance.clampMethod
+
+local function wrapLamp()
+    local LAMP =  scripted_ents.GetStored( "gmod_lamp" ).t
+
+    LAMP.SetLightFov = clampMethod( LAMP.SetLightFov, {values.fov} )
+    LAMP.SetBrightness = clampMethod( LAMP.SetBrightness, {values.brightness} )
+    LAMP.SetDistance = clampMethod( LAMP.SetDistance, {values.distance} )
+
+    print( "[CFC_Tool_Balance] base/lamp loaded" )
+end
+
+local function waitingFor()
+    local ent = scripted_ents.GetStored( "gmod_lamp" )
+    return ent and ent.t.SetDamage ~= nil
+end
+
+local function onTimeout()
+    print( "[CFC_Tool_Balance] base/lamp failed, waiter timed out" )
+end
+
+cfcToolBalance.waitFor( waitingFor, wrapLamp, onTimeout, "base/lamp" )
