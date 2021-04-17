83d7406e7fccf0a46d53547a5b4091c3c6fcab53
pierce <pierce@vulpes.dev>
2021-04-17 00:41:25 -0400

    clamp Setters for turret values

--- lua/tool_balance/tools/wire/turret.lua
+++ lua/tool_balance/tools/wire/turret.lua
@@ -25,6 +25,12 @@ local clampMethod = cfcToolBalance.clampMethod
 local function wrapWireTurret()
     local WIRE_TURRET =  scripted_ents.GetStored( "gmod_wire_turret" ).t
     WIRE_TURRET.Setup = clampMethod( WIRE_TURRET.Setup, values )
+
+	WIRE_TURRET.SetDelay = clampMethod( WIRE_TURRET.SetDelay, {config.delay})
+	WIRE_TURRET.SetForce = clampMethod( WIRE_TURRET.SetForce, {config.force})
+	WIRE_TURRET.SetDamage = clampMethod( WIRE_TURRET.SetDamage, {config.damage})
+	WIRE_TURRET.SetNumBullets = clampMethod( WIRE_TURRET.SetNumBullets, {config.numbullets})
+	WIRE_TURRET.SetSpread = clampMethod( WIRE_TURRET.SetSpread, {config.spread}) 
     print( "[CFC_Tool_Balance] wire/turret loaded" )
 end
 
