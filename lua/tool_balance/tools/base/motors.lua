df6dba757cf816af379ec30f5a9a11afd8d6526a
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-07-22 13:54:17 -0700

    Remove debug print

--- lua/tool_balance/tools/base/motors.lua
+++ lua/tool_balance/tools/base/motors.lua
@@ -7,10 +7,7 @@ entMeta._CFC_ToolBalance_Fire = entMeta._CFC_ToolBalance_Fire or entMeta.Fire
 function entMeta:Fire( input, param, delay, activator, caller )
     if self:GetClass() == "phys_torque" then
         if input == "Scale" then
-            local clamped = math_min( maxScale, param )
-            print( "[CFC_ToolBalance] phys_torque Scale clamped from", param, "to", clamped )
-
-            param = clamped
+            param = math_min( maxScale, param )
         end
     end
 
