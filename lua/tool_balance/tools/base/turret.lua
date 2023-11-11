f3358dd06b55c47bf455285592836918f43f68aa
Redox <mail@redox.to>
2023-11-11 03:07:00 +0100

    Change values (#30)
    * Change values
    
    * Apply to regular ent too and style

--- lua/tool_balance/tools/base/turret.lua
+++ lua/tool_balance/tools/base/turret.lua
@@ -1,10 +1,10 @@
 -- config
 local config = {
     delay      = { min = 0.05, max = math.huge },
-    damage     = { min = 0,   max = 20 },
-    force      = { min = 0,   max = 1 },
-    numbullets = { min = 0,   max = 1 },
-    spread     = { min = 0,   max = 10 }
+    damage     = { min = 0, max = 5 },
+    force      = { min = 0, max = 1 },
+    numbullets = { min = 0, max = 1 },
+    spread     = { min = 0.02, max = 10 }
 }
 
 -- min and max values for gmod_turret
